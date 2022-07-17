13aa6240cd2fb6521bb1a6fcde165730a36d739b
reydez <icyreyeslod@gmail.com>
2022-07-17 16:33:40 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [second, setSecond] = useState("00");
+  const [minute, setMinute] = useState("00");
+  const [isActive, setIsActive] = useState(false);
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    let intervalId;
+
+    if (isActive) {
+      intervalId = setInterval(() => {
+        const secondCounter = counter % 60;
+        const minuteCounter = Math.floor(counter / 60);
+
+        const computedSecond =
+          String(secondCounter).length === 1
+            ? `0${secondCounter}`
+            : secondCounter;
+
+        const computedMinute =
+          String(minuteCounter).length === 1
+            ? `0${minuteCounter}`
+            : minuteCounter;
+
+        setSecond(computedSecond);
+        setMinute(computedMinute);
+
+        setCounter((counter) => counter + 1);
+      }, 1000);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [isActive, counter]);
+
+  const stopTime = () => {
+    setIsActive(false);
+    setCounter(0);
+    setMinute("00");
+    setSecond("00");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="timer-container">
+        <span className="minute">{minute}</span>
+        <span>:</span>
+        <span className="second">{second}</span>
+      </div>
+      <div className="btns-container">
+        <button className="start" onClick={() => setIsActive(!isActive)}>
+          {isActive ? "Pause" : "Play"}
+        </button>
+        <button className="reset" onClick={stopTime}>
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
